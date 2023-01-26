8d8fb652048b46c5f76c07a1855f2dc0fc15a7bd
GL <g.l@gmail.com>
2023-01-26 20:57:05 +0100

    2023-01-26 - Modified LibraryElement - Added Basic Version Of Class Diagram

--- src/main/java/com/elibrary/entities/data/LibraryElement.java
+++ src/main/java/com/elibrary/entities/data/LibraryElement.java
@@ -4,18 +4,62 @@ import java.io.Serializable;
 
 public class LibraryElement extends AbstractElement implements Serializable, Cloneable {
 
+    private long contentID;
+    private long creatorID;
 
+    private boolean contentRestricted;
+    private boolean contentPending;
 
-    protected LibraryElement() {
-
+    public LibraryElement() {
+        setTitle("");
+        this.contentPending = false;
+        this.contentRestricted = false;
+        this.contentID = 0;
+        this.creatorID = 0;
     }
 
-    protected LibraryElement(LibraryElement element) {
+    public LibraryElement(LibraryElement element) {
        setTitle(element.getTitle());
+       setContentID(element.getContentID());
+       setCreatorID(element.getCreatorID());
+       setContentPending(element.isContentPending());
+       setContentRestricted(element.isContentRestricted());
     }
 
-    protected LibraryElement(String title) {
+    public LibraryElement(String title) {
         setTitle(title);
+    }
+
+    public long getContentID() {
+        return contentID;
+    }
+
+    public void setContentID(long contentID) {
+        this.contentID = contentID;
+    }
+
+    public long getCreatorID() {
+        return creatorID;
+    }
+
+    public void setCreatorID(long creatorID) {
+        this.creatorID = creatorID;
+    }
+
+    public boolean isContentRestricted() {
+        return contentRestricted;
+    }
+
+    public void setContentRestricted(boolean contentRestricted) {
+        this.contentRestricted = contentRestricted;
+    }
+
+    public boolean isContentPending() {
+        return contentPending;
+    }
+
+    public void setContentPending(boolean contentPending) {
+        this.contentPending = contentPending;
     }
 
     @Override
